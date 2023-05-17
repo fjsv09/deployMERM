2d285a5bfc57a6f10a3a36c63f42da43ccb1dc0f
Fernando Jose Serrato Vilcherres <fjsv.cjoseph@gmail.com>
2023-05-17 14:56:19 -0500

    imprimir variables

--- server/index.js
+++ server/index.js
@@ -1,6 +1,20 @@
 import app from "./app.js";
-import { PORT } from "./config.js";
+import {
+  PORT,
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE,
+} from "./config.js";
 
 app.listen(PORT);
 
-console.log(`Server is running on port ${PORT}`);
+console.log(
+  `Server is running on port ${PORT}`,
+  DB_HOST,
+  DB_PORT,
+  DB_USER,
+  DB_PASSWORD,
+  DB_DATABASE
+);
